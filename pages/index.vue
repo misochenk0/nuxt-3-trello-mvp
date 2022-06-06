e773626de5ef1e0064539067dee724bbd49a2041
Alexander <misochenkoas@gmail.com>
2022-06-06 10:56:10 +0300

    feat: add ability for users to create workspace cards

--- pages/index.vue
+++ pages/index.vue
@@ -2,12 +2,47 @@
 	<h1>Home page</h1>
 	<h2>Recently viewed</h2>
 	<h2>Workspaces</h2>
+	<input type="text" v-model="newWorkspaceName">
+	<button @click="createWorkspace">Create a workspace</button>
+	<ul class="workspace-list">
+		<li v-for="workspace in workspaceList" :key="workspace.id" class="workspace-card">
+			{{workspace.id}}: {{workspace.name}}
+		</li>
+	</ul>
 </template>
 
-<script>
+<script setup>
+	import {ref} from "vue"
+	const newWorkspaceName = ref("");
+	const workspaceList = ref([
+		{
+			id: 123, name: "test"
+		},
+		{
+			id: 223, name: "Exp"
+		},
+	])
+	const createWorkspace = () => {
+		const randomId = Math.floor(Math.random() * 100);
 
+		workspaceList.value.push({
+			id: randomId,
+			name: newWorkspaceName.value
+		})
+	}
 </script>
 
 <style>
+	.workspace-card {
+		display: block;
+		border: 2px solid #222;
+		border-radius: 4px;
+		padding: 2rem;
+		margin-bottom: 1rem;
+	}
 
+	.workspace-list {
+		margin-left: 0;
+		padding: 0;
+	}
 </style>
